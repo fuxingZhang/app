7a4f77852ffbeb6d6c65bff05c65a512cace8b62
张复星 <star@zhangfuxingdeMacBook-Pro.local>
2020-02-10 11:25:44 +0800

    first commit

---
+++ course/course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  course
+//
+//  Created by 张复星 on 2020/2/10.
+//  Copyright © 2020 张复星. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
